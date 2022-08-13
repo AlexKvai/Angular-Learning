193ba99f524aaf45cfe905e86ef86c264ce666fe
alex parfenov <alexp-2004@mail.ru>
2022-08-14 02:14:17 +0700

    Add routing + finish the project :) It was hard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,25 +9,4 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-  title = 'angular app';
-  // products: IProduct[] = [];
-  loading = false;
-  // products$: Observable<IProduct[]>;
-  term = '';
-
-  constructor(
-    public productsService: ProductService,
-    public modalService: ModalService
-  ) {}
-
-  ngOnInit(): void {
-    this.loading = true;
-    // this.products$ = this.productsService
-    //   .getAll()
-    //   .pipe(tap(() => (this.loading = false)));
-    this.productsService.getAll().subscribe(() => {
-      this.loading = false;
-    });
-  }
-}
+export class AppComponent {}
