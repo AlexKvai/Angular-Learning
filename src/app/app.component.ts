cc58b9842275bd1b0fc803e1f9955240900867fc
alex parfenov <alexp-2004@mail.ru>
2022-08-13 22:25:35 +0700

    Add component + interface for him and change the html + props

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
+import { IProduct } from './models/product';
 import { Component } from '@angular/core';
+import { products as data } from './data/products';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-starting';
+  title = 'angular app';
+
+  products: IProduct[] = data;
 }
