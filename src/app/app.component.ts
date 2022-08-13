9d65b798733fc24f91c28f1fb748c9d988bd736c
alex parfenov <alexp-2004@mail.ru>
2022-08-14 00:01:01 +0700

    Add global error + service and refactoring code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Observable, tap } from 'rxjs';
 import { ProductService } from './services/product.service';
 import { IProduct } from './models/product';
 import { Component, OnInit } from '@angular/core';
@@ -9,14 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'angular app';
-
-  products: IProduct[] = [];
+  // products: IProduct[] = [];
+  loading = false;
+  products$: Observable<IProduct[]>;
 
   constructor(private productsService: ProductService) {}
 
   ngOnInit(): void {
-    this.productsService.getAll().subscribe((products) => {
-      this.products = products;
-    });
+    this.loading = true;
+    this.products$ = this.productsService
+      .getAll()
+      .pipe(tap(() => (this.loading = false)));
+    // this.productsService.getAll().subscribe((products) => {
+    //   this.products = products;
+    //   this.loading = false;
+    // });
   }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,15 +1,27 @@
+import { ErrorService } from './error.service';
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpParams,
+} from '@angular/common/http';
+import { Observable, delay, catchError, throwError } from 'rxjs';
 import { IProduct } from '../models/product';
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private errorService: ErrorService) {}
   getAll(): Observable<IProduct[]> {
-    return this.http.get<IProduct[]>('https://fakestoreapi.com/products', {
-      params: new HttpParams().append('limit', 5),
-    });
+    return this.http
+      .get<IProduct[]>('https://fakestoreapi.com/products', {
+        params: new HttpParams().append('limit', 5),
+      })
+      .pipe(delay(2000), catchError(this.errorHandler.bind(this)));
+  }
+
+  private errorHandler(error: HttpErrorResponse) {
+    this.errorService.handle(error.message);
+    return throwError(() => error.message);
   }
 }
