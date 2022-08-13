05be4fb68d3951ae73811d17f3eaf93c37cdec10
alex parfenov <alexp-2004@mail.ru>
2022-08-13 23:28:16 +0700

    Add async fake api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
+import { ProductService } from './services/product.service';
 import { IProduct } from './models/product';
-import { Component } from '@angular/core';
-import { products as data } from './data/products';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular app';
 
-  products: IProduct[] = data;
+  products: IProduct[] = [];
+
+  constructor(private productsService: ProductService) {}
+
+  ngOnInit(): void {
+    this.productsService.getAll().subscribe((products) => {
+      this.products = products;
+    });
+  }
 }
